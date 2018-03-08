41473ffa5d4616ec765f3d314d2e25d1eaa0b020
Stefan Michaelis <superchampion0521@gmail.com>
2018-03-09 02:48:39 +0400

    Change default package installer to yarn. Create components for landing page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './landing-page/header/header.component';
+import { SponsorsComponent } from './landing-page/sponsors/sponsors.component';
+import { IconSelectorComponent } from './landing-page/icon-selector/icon-selector.component';
+import { PlanComponent } from './landing-page/plan/plan.component';
+import { StatsComponent } from './landing-page/stats/stats.component';
+import { ForumComponent } from './landing-page/forum/forum.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LandingPageComponent,
+        HeaderComponent,
+        SponsorsComponent,
+        IconSelectorComponent,
+        PlanComponent,
+        StatsComponent,
+        ForumComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/landing-page/icon-selector/icon-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-selector',
+  templateUrl: './icon-selector.component.html',
+  styleUrls: ['./icon-selector.component.scss']
+})
+export class IconSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
