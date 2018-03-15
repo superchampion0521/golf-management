f5f9d7e5345afcd29685ec0eb88f224a2577b811
Stefan Michaelis <superchampion0521@gmail.com>
2018-03-15 10:13:18 +0400

    Create alpha version of method component

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -11,6 +11,8 @@ import { StatsComponent } from './stats/stats.component';
 import { ForumComponent } from './forum/forum.component';
 import { KnowledgeComponent } from './knowledge/knowledge.component';
 import { FooterComponent } from './footer/footer.component';
+import { MethodComponent } from './method/method.component';
+import { ProsTestimonialsComponent } from './pros-testimonials/pros-testimonials.component';
 
 @NgModule({
     imports: [
@@ -26,7 +28,9 @@ import { FooterComponent } from './footer/footer.component';
         StatsComponent,
         ForumComponent,
         KnowledgeComponent,
-        FooterComponent
+        FooterComponent,
+        MethodComponent,
+        ProsTestimonialsComponent
     ]
 })
 export class LandingPageModule { }
