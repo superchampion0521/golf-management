916c2bcdc2127681ef0f8bfb57b184f09164a0d1
Stefan Michaelis <superchampion0521@gmail.com>
2018-03-15 20:28:34 +0400

    Create alpha version of frequently-asked component`

---
+++ src/app/landing-page/frequently-asked/frequently-asked.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-frequently-asked',
+  templateUrl: './frequently-asked.component.html',
+  styleUrls: ['./frequently-asked.component.scss']
+})
+export class FrequentlyAskedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
